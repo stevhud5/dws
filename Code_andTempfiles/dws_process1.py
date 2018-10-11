f149d1505bb7fa40d8ccfd9e3fefd1d733031fba
stevhud5 <36199656+stevhud5@users.noreply.github.com>
2018-10-10 23:03:18 -0400

    now working and means updated

--- Code_andTempfiles/dws_process1.py
+++ Code_andTempfiles/dws_process1.py
@@ -29,7 +29,11 @@ Q: We can define a class or not to manage the namespace inside; encapsulation et
 
 #load libraries
 import logging
-import task
+import os.path
+import numpy as np
+import DWS_modules.DWS_DataTreatment_1 as DWS
+import mpl
+#import task
 #import json
 
 # private data shared among methods
@@ -38,7 +42,7 @@ class Private(object):
 private = Private()
 
 #methods
-def initialize(log_text_box, text1, paramload, **kwargs):
+def initialize(log_text_box, paramload, **kwargs):
     # setup the logger and log display box
     # examples of how to use  (I need explanation):
 #    print(logging.getLogger('test'))
@@ -50,28 +54,116 @@ def initialize(log_text_box, text1, paramload, **kwargs):
     sh.setFormatter(formatter)
     private.logger.addHandler(sh)
     private.logger.info('initialize complete')
-    private.task_B = task.Task()
-    private.C = task.TimedTask()
     private.filename_array = []
-    text1.value = 'Process begun!'
+    clear(**kwargs)
 
+def clear_all(data_plot, **kwargs):
+    private.filename_array = []
+    clear(data_plot)
+
+def clear(data_plot, **kwargs):
+    data_plot.clear()
+    data_plot.set_plot_properties(
+        x_label='t(s)',
+        x_scale='log',
+        x_limits=(1e-8,100),
+        y_label='g2_1',
+        dpi=120,
+        aspect_ratio='auto')
+        
+#    data_plot.new_curve('data', line_color='blue', line_style='',
+#                     marker_style='o', marker_color='blue', marker_width=3)
+    data_plot.new_curve('mean', line_color='green', 
+                     marker_style='+', marker_color='green', marker_width=3)
+    
+def array_to_string(f_num_array):
+    text = ''
+    for i in range(0,len(f_num_array)):
+        text = text+'_'+str(f_num_array[i])
+    return text
+    
+def build_mean_fn():
+    #first draft will assume that all in the same folder, just different numbers at the end
+    Na = len(private.filename_array)
+    f_root = []
+    f_ext = []
+    for i in range(0,Na):
+        _f_root,_f_ext = os.path.splitext(private.filename_array[i])
+        f_root.append(_f_root)
+        f_ext.append(_f_ext)
+    f_root_common = os.path.commonprefix(f_root[0:2])
+    Ncommon = len(f_root_common)
+    while f_root_common[Ncommon-1]!='_':
+        Ncommon = Ncommon-1
+    f_root_common = f_root_common[:Ncommon-1]
+    #compute f_num_array
+    f_num_array = []
+    for i in range (0,Na):
+        temp_str=f_root[i]
+        f_num_array.append(temp_str[Ncommon:])
+    fn_out = f_root_common+'_mean'+array_to_string(f_num_array)+f_ext[0]
+    return fn_out
+    
 #do this when file is selected
-def add_to_array(filename_in, **kwargs):  
+def add_to_array(filename_in, filename_out, **kwargs):  
     private.filename_array.append(filename_in.value) 
+    filename_out.value = build_mean_fn()
+    
+def del_from_array(filename_in, filename_out, index, **kwargs):  
+    private.filename_array.pop(int(index.value)) 
+    filename_out.value = build_mean_fn()
     
 #do this when trigger_B button is pressed
-def plot_all(Bstatus, **kwargs): #Bstatus is the indicator that displays the result. 
-    Bstatus.value=private.task_B.do()
+def plot_all(data_plot, **kwargs): #Bstatus is the indicator that displays the result. 
+    maxCRtolerated=500
+    clear(data_plot)
     
-#example repetative action.
-def _count(start_stop, count, dwell_time, **kwargs): 
-    #uses button start_stop, control dwell_time and indicator count
-    _dwell_time = float(dwell_time.value)
-    _dwell_time = _dwell_time/1000.
-    while(start_stop.value):
-        count.value = private.C.do()
-        yield _dwell_time  #the interval between calls.
+    ICF_array = []
+    LagTime_array = []
+    for j in range(0,len(private.filename_array)):
+        fname=private.filename_array[j]
+        MTDuration,EchoDuration,wavelength,T,L,labs,n,R,lstar,CRmean,g2,CRhistory=DWS.loadDWSdata(fname) 
+        LagTime=g2[:,0]
+        ICF=g2[:,1]
+        LagTime_array.append(LagTime)
+        ICF_array.append(ICF)
+        private.logger.info(str(j)+', '+str(T))
+        data_plot.new_curve('data'+str(j), line_color='blue', line_style='',
+                     marker_style='o', marker_color='blue', marker_width=3)
+        data_plot.set_data('data'+str(j),g2)
+    
+    if len(private.filename_array)>1:
+        private.ICF_mean = np.mean(ICF_array, axis=0)
+        ICF_sd = np.std(ICF_array, axis=0)
+        ICF_grad = np.gradient(ICF_array, axis=0)
+        data_plot.set_data('mean',np.column_stack([LagTime, private.ICF_mean]))
+    else:
+        private.ICF_mean = ICF
+    pass
+    
+def write(filename_out, **kwargs):
+    #get header to first file
+    fname=private.filename_array[0]
+    with open(fname, 'r') as f_in:
+        header = ''
+        MTDuration,EchoDuration,wavelength,T,L,labs,n,R,lstar,CRmean,g2,CRhistory=DWS.loadDWSdata(fname) 
+        LagTime=g2[:,0]
+        for index in range (21):
+            header = header+f_in.readline()
         
+    with open(filename_out.value, 'w') as f_out:
+        f_out.write(header)
+        for i in range(len(LagTime)):
+            #f_out.write(LagTime[i]+'\t'+ICF_mean[i]+'\n')
+            f_out.write('%8.6G\t%8.6G\n' % (LagTime[i],private.ICF_mean[i]))
+            
+        f_out.write('\n\nCount Rate  History (kHz) \nElapsed Time (s)\tCount Rate (kHz)\n\n')
+        for i in range(len(CRhistory)):
+            f_out.write('%10.6f\t%10.6f\n' % (CRhistory[i,0],CRhistory[i,1]))
+            pass
+        
+        f_out.write('\nMeasurement notes:\n\n')
+    
     
 def read_config(config_filename_in, paramload, **kwargs):
     try:
