2bd4f5df2484588c871598f5129f478e14a3f107
stevhud5 <36199656+stevhud5@users.noreply.github.com>
2018-10-11 20:23:10 -0400

    change file read (option 'rb') and added new mean data and mra files

--- Code_andTempfiles/DWS_modules/DWS_DataTreatment_1.py
+++ Code_andTempfiles/DWS_modules/DWS_DataTreatment_1.py
@@ -7,9 +7,9 @@ from scipy.signal import savgol_filter
     
   
 def loadDWSdata(path):
-    with open(path) as t_in:
+    with open(path, 'rb') as t_in:
         header_=np.genfromtxt(itertools.islice(t_in, 17),skip_header=6,delimiter='\t',usecols=(1))
-    with open(path) as t_in:
+    with open(path, 'rb') as t_in:
         data_=np.genfromtxt(itertools.islice(t_in, 3000),skip_header=21,delimiter='\t',comments="Count Rate  History (kHz)", skip_footer=4)   
     nanPos_=pd.isnull(data_).any(1).nonzero()[0]     # get all positions of NaN 
     g2_=data_[0:min(nanPos_)]               # save valid part of g2 (ignore NaN)
@@ -19,7 +19,7 @@ def loadDWSdata(path):
     
 
 def loadDWSdataMR(path):
-    with open(path) as t_in:
+    with open(path, 'rb') as t_in:
         header_=np.genfromtxt(itertools.islice(t_in, 16),skip_header=5,delimiter='\t',usecols=(1))
     data_=np.loadtxt(path,skiprows=21,delimiter='\t',)  
     return (header_[0],header_[1],header_[2],header_[3],header_[4],header_[5],header_[7],header_[8],header_[10],header_[9],data_)
